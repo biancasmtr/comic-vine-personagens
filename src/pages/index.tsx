67bd84f7fa8e6c7f3bd82e65372d8f7e5838a3de
Bianca Monteiro <biancasouzamonteirogmail.com>
2023-05-12 10:47:18 -0300

    mudança de estilo na pagina inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,82 +30,47 @@ const Home: FC<HomeProps> = ({ characters }) => {
     c.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  // return (
-  //   <div className="container mx-auto">
-  //     <div className="flex flex-col items-center py-10">
-  //       <div className="text-3xl font-bold underline mb-4">
-  //         <div className="flex flex-row items-center">
-  //           <input
-  //             type="text"
-  //             placeholder="Pesquisar personagem"
-  //             value={searchTerm}
-  //             onChange={(e) => setSearchTerm(e.target.value)}
-  //             className="px-2 py-1 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-  //           />
-  //           <button
-  //             className="px-4 py-2 ml-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-  //             onClick={() => setSearchTerm("")}
-  //           >
-  //             Limpar
-  //           </button>
-  //         </div>
-  //       </div>
-  //       {filteredCharacters.length === 0 ? (
-  //         <div className="text-lg font-bold text-red-500">Lista vazia!</div>
-  //       ) : (
-  //         <div className="grid grid-cols-3 gap-4">
-  //           {filteredCharacters.map((c: { id: number; name: string }, index: number) => (
-  //             <div
-  //               key={index}
-  //               className="border border-gray-200 rounded-lg shadow-md hover:shadow-lg cursor-pointer transition duration-300 ease-in-out"
-  //             >
-  //               <Link href={`/character/${c.id}`}>
-  //                 <span className="block p-4 text-lg font-bold text-blue-600 hover:underline">{c.name} </span>
-  //               </Link>
-  //             </div>
-  //           ))}
-  //         </div>
-  //       )}
-  //     </div>
-  //   </div>
-  // );  
-
   return (
-    <div className="container mx-auto">
-      <div className="flex flex-col items-center py-10">
-        <div className="text-3xl font-bold underline mb-4">
-          <div className="flex flex-row items-center">
-            <input
-              type="text"
-              placeholder="Pesquisar personagem"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent shadow-md"
-            />
-            <button
-              className="px-6 py-2 ml-2 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:bg-blue-600 shadow-md"
-              onClick={() => setSearchTerm("")}
-            >
-              Limpar
-            </button>
+    <div className="bg-gray-200 h-screen">
+      <div className="container mx-auto py-12">
+        <div className="bg-white rounded-lg shadow-lg px-8 py-10">
+          <div className="text-center mb-8">
+            <h1 className="text-4xl font-bold text-gray-800 underline">Pesquisar personagem</h1>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="w-1/2 flex items-center border-b border-gray-400 py-2 mb-6">
+              <input
+                type="text"
+                placeholder="Digite o nome do personagem"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+              />
+              <button
+                className="bg-red-700 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                onClick={() => setSearchTerm("")}
+              >
+                Limpar
+              </button>
+            </div>
+            {filteredCharacters.length === 0 ? (
+              <div className="text-lg font-bold text-red-500">Lista vazia!</div>
+            ) : (
+              <div className="grid grid-cols-3 gap-4">
+                {filteredCharacters.map((c: { id: number; name: string }, index: number) => (
+                  <div
+                    key={index}
+                    className="border border-gray-200 rounded-lg shadow-md hover:shadow-lg cursor-pointer transition duration-300 ease-in-out"
+                  >
+                    <Link href={`/character/${c.id}`}>
+                      <span className="block p-4 text-lg font-bold text-red-700 hover:underline">{c.name} </span>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         </div>
-        {filteredCharacters.length === 0 ? (
-          <div className="text-lg font-bold text-red-500">Lista vazia!</div>
-        ) : (
-          <div className="grid grid-cols-3 gap-4">
-            {filteredCharacters.map((c: { id: number; name: string }, index: number) => (
-              <div
-                key={index}
-                className="border border-gray-200 rounded-lg shadow-md hover:shadow-lg cursor-pointer transition duration-300 ease-in-out"
-              >
-                <Link href={`/character/${c.id}`}>
-                  <span className="block p-4 text-lg font-bold text-blue-600 hover:underline">{c.name}</span>
-                </Link>
-              </div>
-            ))}
-          </div>
-        )}
       </div>
     </div>
   );
