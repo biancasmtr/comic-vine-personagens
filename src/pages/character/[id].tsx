81283944f2f27e4d504af5e049c90666c859e8a1
Bianca Monteiro <bianca_souza_monteiro@hotmail.com>
2023-05-15 15:03:46 -0300

    melhoria de layout final - tela inicial com grid/lista, exibir todas as imgs do personagem na pag de detalhes e correção de campo de busca

--- src/pages/character/[id].tsx
+++ src/pages/character/[id].tsx
@@ -1,11 +1,11 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import axios from "axios";
 import { GetServerSideProps } from "next";
-import { useRouter } from "next/router";
 import Link from "next/link";
 
 const urlBase = "https://comicvine.gamespot.com/api/";
-const route = "character/4005";
+const route = "character";
+const prefixNumber = 4005;
 const apiKey = "2ae15e78ab553e040b833132f5a39218a57cccab";
 const format = "json";
 
@@ -16,6 +16,7 @@ type CharacterProps = {
     real_name: string;
     aliases: string;
     gender: number;
+    birth: string;
     image: {
       original_url: string;
     };
@@ -23,7 +24,15 @@ type CharacterProps = {
 };
 
 const Character: FC<CharacterProps> = ({ character }) => {
-  const router = useRouter();
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handlePreviousImage = () => {
+    setCurrentImageIndex((prevIndex) => prevIndex - 1);
+  };
+
+  const handleNextImage = () => {
+    setCurrentImageIndex((prevIndex) => prevIndex + 1);
+  };
 
   return (
     <div className="container mx-auto py-8 px-8 min-h-100vh">
@@ -38,22 +47,57 @@ const Character: FC<CharacterProps> = ({ character }) => {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-center">
           <div className="md:mr-10 mb-6 md:mb-0 flex justify-center">
-            <img
-              src={character.image.original_url}
-              alt={character.name}
-              className="rounded-lg shadow-md"
-              width={300}
-            />
+            {character.image && (
+              <>
+                <img
+                  src={character.image.original_url}
+                  alt={character.name}
+                  className="rounded-lg shadow-md"
+                  width={300}
+                />
+                <div className="flex justify-between mb-2">
+                  {currentImageIndex > 0 && (
+                    <button
+                      className="bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-4 rounded"
+                      onClick={handlePreviousImage}
+                    >
+                      Anterior
+                    </button>
+                  )}
+                  {currentImageIndex < character.image.length - 1 && (
+                    <button
+                      className="bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-4 rounded"
+                      onClick={handleNextImage}
+                    >
+                      Próxima
+                    </button>
+                  )}
+                </div>
+              </>
+            )}
           </div>
           <div className="text-left">
             <div className="mb-4">
               <strong className="mr-2">Nome real:</strong>
-              {character.real_name == null ? <span className="text-red-500">Não possui essa informação</span> : character.real_name}
+              {character.real_name == null ? (
+                <span className="text-red-500">Não possui essa informação</span>
+              ) : (
+                character.real_name
+              )}
             </div>
-
             <div className="mb-4">
               <strong className="mr-2">Outros nomes:</strong>
-              {character.aliases == null ? "Não possui este dado" : character.aliases}
+              {character.aliases == null ? (
+                <span>Não possui este dado</span>
+              ) : (character.aliases)}
+            </div>
+            <div className="mb-4">
+              <strong className="mr-2">Data de Nascimento:</strong>
+              {character.birth == null ? (
+                <span className="text-red-500">Não possui essa informação</span>
+              ) : (
+                character.birth
+              )}
             </div>
             <div className="mb-4">
               <strong className="mr-2">Gênero:</strong>
@@ -62,10 +106,10 @@ const Character: FC<CharacterProps> = ({ character }) => {
           </div>
         </div>
         <button className="mb-4 px-8 py-2 mt-8 text-white bg-red-700 rounded-lg hover:bg-red-800 focus:outline-none focus:bg-red-800 block mx-auto w-full md:w-auto">
-  <Link href="/">
-    <span>Voltar</span>
-  </Link>
-</button>
+          <Link href="/">
+            <span>Voltar</span>
+          </Link>
+        </button>
       </div>
     </div>
   );
@@ -76,8 +120,9 @@ export default Character;
 
 export const getServerSideProps: GetServerSideProps = async (param) => {
   const { id } = param.query;
+  const route = `character/${prefixNumber}-${id}`;
   const response = await axios.get(
-    `${urlBase}${route}-${id}?api_key=${apiKey}&format=${format}`
+    `${urlBase}${route}?api_key=${apiKey}&format=${format}`
   );
   const character = response.data.results;
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
-import { FC, useState } from "react";
+import { FC, useState, useEffect } from "react";
 import axios from "axios";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import { FiList, FiGrid } from "react-icons/fi";
 
 const urlBase = 'https://comicvine.gamespot.com/api/';
 const route = 'characters';
@@ -23,12 +24,50 @@ type HomeProps = {
   characters: any;
 };
 
+enum ViewType {
+  List = "list",
+  Grid = "grid",
+}
+
 const Home: FC<HomeProps> = ({ characters }) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [viewType, setViewType] = useState(ViewType.Grid);
+  const [displayedCharacters, setDisplayedCharacters] = useState(characters);
+  const [showLoadMore, setShowLoadMore] = useState(false);
+
+  const handleToggleView = () => {
+    setViewType((prevType) =>
+      prevType === ViewType.List ? ViewType.Grid : ViewType.List
+    );
+  };
+
+  const handleLoadMore = () => {
+    const filtered = characters.filter((c: { name: string }) =>
+      c.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setDisplayedCharacters(filtered);
+    setShowLoadMore(false);
+  };
+
+  const handleClearSearch = () => {
+    setSearchTerm("");
+    setDisplayedCharacters(characters);
+    setShowLoadMore(false);
+  };
 
   const filteredCharacters = characters.filter((c: { name: string }) =>
     c.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
+  useEffect(() => {
+    if (searchTerm) {
+      setDisplayedCharacters(filteredCharacters.slice(0, 4));
+      setShowLoadMore(true);
+    } else {
+      setDisplayedCharacters(characters);
+      setShowLoadMore(false);
+    }
+  }, [searchTerm]);
 
   return (
     <div className="bg-gray-200 h-screen">
@@ -41,38 +80,69 @@ const Home: FC<HomeProps> = ({ characters }) => {
       <div className="container mx-auto py-8">
         <div className="bg-white rounded-lg shadow-lg px-8 py-10">
           <div className="flex flex-col items-center">
-            <div className="w-1/2 flex items-center border-b border-gray-400 py-2 mb-6">
-              <input
-                type="text"
-                placeholder="Digite o nome do personagem"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-              />
-              <button
-                className="bg-red-700 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                onClick={() => setSearchTerm("")}
-              >
-                Limpar
-              </button>
+            <div className="flex w-full">
+            <div className="flex w-96"></div>
+              <div className="w-96 flex items-center  border-b border-gray-400 mb-6">
+                <input
+                  type="text"
+                  placeholder="Digite o nome do personagem"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none text-sm"
+                />
+                <button
+                  className="bg-red-700 hover:bg-red-700 text-white font-bold py-2 px-4 ml-2 mb-2 rounded focus:outline-none focus:shadow-outline"
+                  onClick={handleClearSearch}
+                >
+                  Limpar
+                </button>
+              </div>
+              <div className="flex justify-end mb-8 mr-8 mt-2 ml-auto">
+                {viewType === ViewType.Grid ? (
+                  <button
+                    className="text-gray-600 hover:text-red-700"
+                    onClick={() => setViewType(ViewType.List)}
+                  >
+                    <FiList size={32} />
+                  </button>
+                ) : (
+                  <button
+                    className="text-gray-600 hover:text-red-700"
+                    onClick={() => setViewType(ViewType.Grid)}
+                  >
+                    <FiGrid size={32} />
+                  </button>
+                )}
+              </div>
             </div>
-            <div className="text-lg font-bold text-gray-700 mb-4">
+
+            <div className="text-base font-bold text-gray-700 mb-6">
               {filteredCharacters.length} personagens encontrados
             </div>
-            {filteredCharacters.length === 0 ? (
+            {displayedCharacters.length === 0 ? (
               <div className="text-lg font-bold text-red-500">Lista vazia!</div>
             ) : (
-              <div className="grid grid-cols-3 gap-4">
-                {filteredCharacters.map((c: { id: number; name: string }, index: number) => (
+              <div className={`grid ${viewType === ViewType.List ? "grid-cols-1" : "grid-cols-4"} gap-4 ${viewType === ViewType.List ? "w-full" : ""}`}>
+                {displayedCharacters.map((c: { id: number; name: string }, index: number) => (
                   <div
                     key={index}
-                    className="border border-gray-200 rounded-lg shadow-md hover:shadow-lg cursor-pointer transition duration-300 ease-in-out"
+                    className={`border border-gray-200 rounded-lg shadow-md hover:shadow-lg cursor-pointer transition duration-300 ease-in-out ${viewType === ViewType.List ? "text-center" : ""}`}
                   >
                     <Link href={`/character/${c.id}`}>
-                      <span className="block p-4 text-lg font-bold text-red-700 hover:underline">{c.name} </span>
+                      <span className="block p-4 text-lg font-bold text-red-700 hover:underline">{c.name}</span>
                     </Link>
                   </div>
                 ))}
+              </div>
+            )}
+            {showLoadMore && (
+              <div className="flex justify-center mt-4">
+                <button
+                  className="mb-4 px-8 py-2 mt-8 text-white bg-red-700 rounded-lg hover:bg-red-800 focus:outline-none focus:bg-red-800 block mx-auto w-full md:w-auto"
+                  onClick={handleLoadMore}
+                >
+                  Carregar Mais
+                </button>
               </div>
             )}
           </div>
@@ -80,6 +150,7 @@ const Home: FC<HomeProps> = ({ characters }) => {
       </div>
     </div>
   );
+
 
 };
 
